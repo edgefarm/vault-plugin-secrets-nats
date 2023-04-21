21fb7ded17ceea187be6eeb9fba63f535b49ee93
Armin Schlegel <armin.schlegel@gmx.de>
2023-04-21 09:37:37 +0200

    fix: fixed CI workflow
    Signed-off-by: Armin Schlegel <armin.schlegel@gmx.de>

---
+++ ci/semantic-release-patch-readme.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+NEXTVERSION=$1
+sha256sum build/vault/plugins/vault-plugin-secrets-nats > build/vault/plugins/vault-plugin-secrets-nats.sha256
+export SHA256SUM=$(cat build/vault/plugins/vault-plugin-secrets-nats.sha256 | cut -d ' ' -f1)
+sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" README.md
+sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" README.md
