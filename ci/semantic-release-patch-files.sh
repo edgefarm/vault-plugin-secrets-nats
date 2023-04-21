18ddcad111c871d0aeba0a7e63f7015adafb2a21
Armin Schlegel <armin.schlegel@gmx.de>
2023-04-21 09:51:21 +0200

    fix: updated CI for dev environment

--- ci/semantic-release-patch-files.sh
+++ ci/semantic-release-patch-files.sh
@@ -5,3 +5,5 @@ sha256sum build/vault/plugins/vault-plugin-secrets-nats > build/vault/plugins/va
 export SHA256SUM=$(cat build/vault/plugins/vault-plugin-secrets-nats.sha256 | cut -d ' ' -f1)
 sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" README.md
 sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" README.md
+sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" dev/manifests/vault/vault.yaml
+sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" dev/manifests/vault/vault.yaml
