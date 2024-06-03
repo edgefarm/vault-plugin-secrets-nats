54022da0a159d4ce0726ab8a0ccf7fb1e571b45e
Kristoffer <123738040+zs-ko@users.noreply.github.com>
2024-06-03 14:13:20 +0200

    feat: add multiarch build using matrix strategy (#29)
    * feat: add multiarch build using matrix strategy
    
    ---------
    
    Co-authored-by: zs-ko <zs-ko@users.noreply.github.com>

--- ci/semantic-release-patch-files.sh
+++ ci/semantic-release-patch-files.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -e
 NEXTVERSION=$1
-sha256sum build/vault/plugins/vault-plugin-secrets-nats > build/vault/plugins/vault-plugin-secrets-nats.sha256
+for file in build/vault/plugins/vault-plugin-secrets-nats-*; do
+  sha256sum $file > $file.sha256
+    # this is to retain backward compatibility with the old naming convention
+    if echo "$file" | grep -q "amd64"; then
+        cp $file ./build/vault/plugins/vault-plugin-secrets-nats
+        cp $file.sha256 build/vault/plugins/vault-plugin-secrets-nats.sha256
+    fi
+done
+# only use x86 for the README.md and dev/manifests/vault/vault.yaml
 export SHA256SUM=$(cat build/vault/plugins/vault-plugin-secrets-nats.sha256 | cut -d ' ' -f1)
 sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" README.md
 sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" README.md
