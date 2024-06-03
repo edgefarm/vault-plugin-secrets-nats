85ab55e99de02f42d18f323e7884a14583b5d781
zs-ko <zs-ko@users.noreply.github.com>
2024-06-03 13:31:12 +0200

    ci: revert changee for generate.go. not needed to force arch

--- generate.go
+++ generate.go
@@ -1,10 +1,10 @@
-//go:build (generate && linux && amd64) || (generate && darwin && amd64)
-// +build generate,linux,amd64 generate,darwin,amd64
+//go:build generate
+// +build generate
 
 package natsbackend
 
 // Generate deepcopy methodsets
-//go:generate env GOARCH=amd64 go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=./hack/boilerplate.go.txt paths=./...
+//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=./hack/boilerplate.go.txt paths=./...
 
 import (
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen" //nolint:typecheck
